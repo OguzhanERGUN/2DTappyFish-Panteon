a40318c0cb4a0ad3fddc9f8dc8cab7876f1ddaf4
Oğuzhan Ergün <Oguzh4n15@hotmail.com>
2023-05-24 20:55:38 +0300

    Controller Created
    Controller Created

---
+++ Assets/Scripts/FishController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishController : MonoBehaviour
+{
+
+    private Rigidbody2D rbFish;
+    [SerializeField]private float speed;
+    int angle;
+    int maxAngle = 20;
+    int minAngle = -60;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbFish = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rbFish.velocity = new Vector2(rbFish.velocity.x, speed);
+        }
+        if (rbFish.velocity.y >= 0)
+        {
+            if (angle <= maxAngle)
+            {
+                angle = angle + 4;
+            }
+        }
+        else if (rbFish.velocity.y < -2.5f)
+        {
+            if (angle >= minAngle)
+            {
+                angle = angle - 2;
+            }
+        }
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
